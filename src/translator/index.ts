1f8bf2cd66900f6bd039aeb4b136eb7251143f18
helcu <helcu@hotmail.com>
2023-12-15 01:29:20 -0500

    add: doc-strings and comments

--- src/translator/index.ts
+++ src/translator/index.ts
@@ -2,85 +2,89 @@ import mongoParse from "mongo-parse";
 import JSON5 from "json5";
 import { operatorsMap } from "./operationsMap.js";
 
+// Constant representing the nested operator
 const NESTED_OPERATOR = "NESTED_OPERATOR";
 
+// Define the structure of parsed MongoDB query result
 type ParsedResult = {
   fromSection: string;
   whereSection: Array<any>;
   selectSection: Array<any>;
 };
 
+// Define the structure of a parser element
+type ParserElement = {
+  field?: string;
+  operator: string;
+  operand: Array<any>;
+};
+
+// Translator class responsible for converting MongoDB query to SQL
 export class Translator {
+  /**
+   * Convert MongoDB query to SQL.
+   * @param input MongoDB query string
+   * @returns SQL query string
+   */
   static convertToSQL(input: string): string {
     const parsedObject = Translator.parseSQLInput(input);
     return Translator.buildQueryString(parsedObject);
   }
 
+  /**
+   * Parse the input MongoDB query to extract components.
+   * @param query MongoDB query string
+   * @returns Parsed components (FROM, WHERE, SELECT) of the query
+   */
   private static parseSQLInput(query: string): ParsedResult {
-    if (query.length < 1) {
-      throw Error("Empty query statement");
+    if (!query.startsWith("db.")) {
+      throw new Error("Wrong format, use db statement");
     }
 
-    let index = query.indexOf(".");
-    if (index === -1) {
-      throw Error("Wrong format, use .");
-    }
-    const prefix = query.slice(0, index);
-    if (prefix !== "db") {
-      throw Error("Wrong format, use db statement");
+    // Extract parts of the query
+    const parts = query.split(".");
+    if (parts.length < 3 || !parts[2].startsWith("find(")) {
+      throw new Error("Wrong format or unsupported MongoDB method");
     }
 
-    let rest = query.slice(index + 1);
-    index = rest.indexOf(".");
-    if (index === -1) {
-      throw Error("Wrong format");
-    }
-    const fromSection = rest.slice(0, index);
-    if (fromSection.length == 0) {
-      throw Error("Missing collection name");
-    }
+    // Extract sections: FROM, WHERE, SELECT
+    const fromSection = parts[1];
+    const rest = parts.slice(2).join(".");
 
-    rest = rest.slice(index + 1);
-    index = rest.indexOf("(");
-    const method = rest.slice(0, index);
-    if (method !== "find") {
-      throw Error("Wrong or not supported MongoDB method (" + method + ")");
+    // Validate method parenthesization
+    const openParenIndex = rest.indexOf("(");
+    const closeParenIndex = rest.lastIndexOf(")");
+    if (
+      openParenIndex === -1 ||
+      closeParenIndex === -1 ||
+      closeParenIndex <= openParenIndex
+    ) {
+      throw new Error("Method is not parenthesized properly");
     }
 
-    let lastChar = rest.length;
-    if (rest[lastChar - 1] === ";") {
-      lastChar -= 1;
+    // Extract and parse MongoDB query
+    const mongoQuery = rest.substring(openParenIndex + 1, closeParenIndex);
+    let preparedForObjectParsing = "";
+    try {
+      preparedForObjectParsing = JSON5.parse("[" + mongoQuery + "]");
+    } catch (error) {
+      throw new Error("Error parsing MongoDB query");
     }
-    const mongoQuery = rest.slice(index, lastChar);
 
-    if (mongoQuery[0] !== "(" || mongoQuery[mongoQuery.length - 1] !== ")") {
-      throw Error("Method is not parenthized");
-    }
+    // Parse WHERE and SELECT sections using mongo-parse library
+    const whereParsed = mongoParse.parse(preparedForObjectParsing[0]);
+    const selectParsed = mongoParse.parse(preparedForObjectParsing[1]);
 
-    const preparedForObjectParsing =
-      "[" + mongoQuery.slice(1, mongoQuery.length - 1) + "]";
-    console.log("PARSED OBEJCT", JSON.stringify(preparedForObjectParsing));
-    const queryObject = JSON5.parse(preparedForObjectParsing);
-
-    const whereParsed = mongoParse.parse(queryObject[0]);
-
-    const selectParsed = mongoParse.parse(queryObject[1]);
-
-    const whereSection = whereParsed.parts.reduce(
-      (prev: any, curr: any) => [...prev, Translator.buildWhereSection(curr)],
-      []
+    // Build WHERE and SELECT sections
+    const whereSection = whereParsed.parts.map((part: any) =>
+      Translator.buildWhereSection(part)
     );
-
-    const selectSection = selectParsed.parts.reduce(
-      (prev: any, curr: { operand: number; field: any }) => {
-        if (curr.operand === 1) {
-          return [...prev, curr.field];
-        } else {
-          return null;
-        }
-      },
-      []
-    );
+    const selectSection = selectParsed.parts.reduce((prev: any, curr: any) => {
+      if (curr.operand === 1) {
+        return [...prev, curr.field];
+      }
+      return prev;
+    }, []);
 
     return {
       fromSection,
@@ -89,14 +93,21 @@ export class Translator {
     };
   }
 
-  private static buildWhereSection(currentMongoParserElement: any): any {
-    const { field, operator, operand } = currentMongoParserElement;
+  /**
+   * Build the WHERE section of SQL query from parsed MongoDB WHERE section.
+   * @param parserElement Parsed MongoDB element for WHERE clause
+   * @returns Constructed WHERE clause elements for SQL query
+   */
+  private static buildWhereSection(parserElement: ParserElement): any {
+    const { field, operator, operand } = parserElement;
 
-    if (typeof field === "undefined") {
-      const nested = operand.reduce((prev: any, curr: any) => {
+    // Build WHERE clause elements based on MongoDB parsed elements
+    if (field === undefined) {
+      // Handle nested conditions and build appropriate structure
+      const nested = operand.map((curr: any) => {
         const parsed = mongoParse.parse(curr);
-        return [...prev, Translator.buildWhereSection(parsed.parts[0])];
-      }, []);
+        return Translator.buildWhereSection(parsed.parts[0]);
+      });
 
       return {
         field: NESTED_OPERATOR,
@@ -104,6 +115,7 @@ export class Translator {
         operand: nested,
       };
     }
+
     return {
       field,
       operator: operatorsMap[operator],
@@ -111,24 +123,33 @@ export class Translator {
     };
   }
 
+  /**
+   * Build the complete SQL query string.
+   * @param parsedResult Parsed MongoDB query components
+   * @returns Assembled SQL query string
+   */
   private static buildQueryString(parsedResult: ParsedResult): string {
     const { selectSection, fromSection, whereSection } = parsedResult;
 
+    // Construct SQL query components: SELECT, FROM, WHERE
     const whereClauseSQL = whereSection
-      .reduce(
-        (prev: any, curr: any) => [...prev, Translator.buildWhereString(curr)],
-        []
-      )
+      .map((curr: any) => Translator.buildWhereString(curr))
       .join(" AND ");
 
-    const select =
-      "SELECT " + (selectSection.length > 0 ? selectSection.join(", ") : "*");
-    const from = "FROM " + fromSection;
-    const where = "WHERE " + whereClauseSQL;
+    const select = `SELECT ${
+      selectSection.length > 0 ? selectSection.join(", ") : "*"
+    }`;
+    const from = `FROM ${fromSection}`;
+    const where = `WHERE ${whereClauseSQL}`;
 
-    return select + " " + from + " " + where + ";";
+    return `${select} ${from} ${where};`;
   }
 
+  /**
+   * Build the WHERE clause string for a single element.
+   * @param elem Single element from parsed MongoDB WHERE section
+   * @returns Constructed WHERE clause string for SQL query
+   */
   private static buildWhereString(elem: {
     field: any;
     operator: any;
@@ -136,37 +157,40 @@ export class Translator {
   }): string {
     const { field, operator, operand } = elem;
 
+    // Construct WHERE clause for a single element based on its field, operator, and operand
+    const typpedOperand = Translator.getTyppedOperand(operand, operator, field);
+
     if (field === NESTED_OPERATOR) {
-      return "(" + Translator.getTyppedOperand(operand, operator, field) + ")";
+      return `(${typpedOperand})`;
     } else {
-      return (
-        field +
-        " " +
-        operator +
-        " " +
-        Translator.getTyppedOperand(operand, operator, field)
-      );
+      return `${field} ${operator} ${typpedOperand}`;
     }
   }
 
+  /**
+   * Get typed operand for WHERE clause.
+   * @param operand Operand value
+   * @param operator Operator for WHERE clause
+   * @param field Field type for WHERE clause
+   * @returns Typed operand value for WHERE clause
+   */
   private static getTyppedOperand(
     operand: any,
     operator: any,
     field: any
   ): any {
+    // Determine the operand type and format it accordingly for WHERE clause
+    // Handle strings, arrays (for IN operator), and nested conditions
     if (typeof operand === "string") {
-      return "'" + operand + "'";
-    } else if (operator === "IN") {
-      operand = operand
-        .map((op: any) => Translator.getTyppedOperand(op, null, null))
-        .join(", ");
-      return "(" + operand + ")";
-    } else if (field === NESTED_OPERATOR) {
+      return `'${operand}'`;
+    } else if (Array.isArray(operand) && operator === "IN") {
+      return `(${operand
+        .map((op) => Translator.getTyppedOperand(op, null, null))
+        .join(", ")})`;
+    } else if (field === NESTED_OPERATOR && Array.isArray(operand)) {
       return operand
-        .reduce((prev: any, curr: any) => {
-          return [...prev, Translator.buildWhereString(curr)];
-        }, [])
-        .join(" " + operator + " ");
+        .map((curr) => Translator.buildWhereString(curr))
+        .join(` ${operator} `);
     } else {
       return operand;
     }
